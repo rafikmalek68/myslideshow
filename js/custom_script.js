a8ad811db50b401d65f899b7bf3723fb42bd2bcf
fz <fz@fz-PC>
2017-06-03 04:23:59 -0400

    main file added

---
+++ js/custom_script.js
@@ -0,0 +1,58 @@
+jQuery(document).ready(function($) {
+
+	// Uploading files
+	var file_frame;
+
+	jQuery.fn.upload_listing_image = function( button ) {
+		var button_id = button.attr('id');
+		var field_id = button_id.replace( '_button', '' );
+
+		// If the media frame already exists, reopen it.
+		if ( file_frame ) {
+		  file_frame.open();
+		  return;
+		}
+
+		// Create the media frame.
+		file_frame = wp.media.frames.file_frame = wp.media({
+		  title: jQuery( this ).data( 'uploader_title' ),
+		  button: {
+		    text: jQuery( this ).data( 'uploader_button_text' ),
+		  },
+		  multiple: true
+		});
+
+		// When an image is selected, run a callback.
+		file_frame.on( 'select', function() {
+                    
+		  //var attachment = file_frame.state().get('selection').first().toJSON();
+                  var attachment = file_frame.state().get('selection').toJSON();
+                  
+                  var images_html='';
+                  for(var i=0;i<attachment.length;i++){
+                      
+                      images_html +='<div id="imageid_'+attachment[i].id+'">'
+                      images_html +='<img src="'+attachment[i].url+'" style="width:250px;hight:auto;border:0;display:none;" />';
+                      images_html +='<input type="hidden" id="hidden_imgid_'+attachment[i].id+'" name="ImageIds[]" value="'+attachment[i].id+'" />';
+                      images_html +='<p class="hide-if-no-js"><a title="" class="remove_img" href="javascript:;"  id="remove_'+attachment[i].id+'" >Remove</a></p>';
+                      images_html +='</div>';
+                  }
+                    jQuery("#slideimage_contenar").append(images_html);
+                    jQuery( '#listingimagediv img' ).show();
+		});
+
+		// Finally, open the modal
+		file_frame.open();
+	};
+
+	jQuery('#listingimagediv').on( 'click', '#upload_listing_image_button', function( event ) {
+		event.preventDefault();
+		jQuery.fn.upload_listing_image( jQuery(this) );
+	});
+        
+        jQuery('#listingimagediv').on( 'click', '.remove_img', function( event ) {
+		event.preventDefault();
+                jQuery(this).parent().parent().remove();
+	});
+
+});
